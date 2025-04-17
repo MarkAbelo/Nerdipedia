8c43eb22e964e658af3c0b674b83dafea2919cdc
MAbeloStevens <124626324+MAbeloStevens@users.noreply.github.com>
2025-04-17 18:16:40 -0400

    added react-router-dom

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/Nerdipedia_BGless.png'
 import viteLogo from '/icon.svg'
 import './App.css'
 
