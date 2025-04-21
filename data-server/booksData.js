3dbe58b4aadd47280e139b0748ffb99e20c8decb
KP0823 <kushparmar0823@gmail.com>
2025-04-21 13:23:25 -0400

    Added skeletons of book data functions, added Mark getBookCard function

--- data-server/booksData.js
+++ data-server/booksData.js
@@ -1,6 +1,6 @@
 import validationFunctions from "./validation.js";
 import { ObjectId } from "mongodb";
-import { reviews as reviewsCollection } from './config/mongoCollections.js'
+import { reviews as reviewsCollection, accounts as accountsCollection } from './config/mongoCollections.js'
 import axios from "axios";
 
 const booksData={
@@ -8,7 +8,7 @@ const booksData={
     getId (workKey){
         if (!workKey) throw ('No key provided');
         if(typeof workKey!== 'string') throw ('Key must be type string');
-        return workKey.split('/works/')[1] || ''; // Safely handle missing keys
+        return workKey.split('/works/')[1] || '';
     },
 
     async getBook (id){
@@ -28,8 +28,30 @@ const booksData={
         const reviews = await reviewsCollection()
         const bookReviewList= await reviews.find ({forID: id, section:'book'}).toArray();
         let bookReview;
+        //if there are no reviews 
         if(bookReviewList.length===0){
             bookReview = 'There are no reviews';
+        }
+        //if there are reviews
+        else{
+            const posterIDs = bookReviewList.map(review => review.posterID);
+            const posterObjectIDs = posterIDs.map(id => new ObjectId(id));
+            //grab user names 
+            const accounts = await accountsCollection
+            const accountList = await accounts.find({
+                _id: { $in: posterObjectIDs } 
+              }, {
+                projection: { username: 1 }
+              }).toArray();
+
+            const accountMap = new Map();
+            accountList.forEach(account => {
+                accountMap.set(account._id.toString(), account.username);
+            });
+            bookReview = bookReviewList.map(review => ({
+                ...review,
+                username: accountMap.get(review.posterID.toString()) || 'Deleted User'
+            }));
         }
         //put the reviews in
         bookInfo['bookReview']=bookReview;
@@ -69,11 +91,44 @@ const booksData={
                     id: this.getId(book.key),
                     title:book.title,
                     authors: book.author_name || [],
+                    //-S, -M, -L for image size
                     cover: book.cover_i? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` :null,
                     publish_year:book.first_publish_year
                 }
             )
         )
-    } 
+    },
+
+    async getBookCard(id){
+        if(!id) throw('Id must be provided!')
+        //the book ids look like this: OL27448W 
+        id= await validationFunctions.validString(id,"book");
+        // might need to run this function depending how the id is given
+        //id = this.getId(book.key)  
+        let bookInfo;
+        try{
+            const bookResponse = await axios.get(`https://openlibrary.org/works/${id}.json`);
+            bookInfo=bookResponse.data;
+        }
+        catch(e){
+            throw (e);
+        }
+        //-S, -M, -L for image size
+        return {title: bookInfo.title, cover: bookInfo.covers[0]? `https://covers.openlibrary.org/b/id/${bookInfo.covers[0]}-M.jpg` :null }
+
+    },
+    async recommendBooks(){
+
+    },
+    async popularPosts(){
+
+    },
+    async getRecentPosts(){
+
+    },
+    async searchPosts(){
+        
+    }
+
 }
 export default booksData
