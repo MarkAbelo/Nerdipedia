c2d989054057aad1912a8e334f1e36439867634f
MAbeloStevens <124626324+MAbeloStevens@users.noreply.github.com>
2025-04-22 14:21:11 -0400

    more routing skeleton

--- data-server/routes/index.js
+++ data-server/routes/index.js
@@ -1,6 +1,16 @@
+import accountRoutes from './accounts.js';
+import postRoutes from './posts.js';
+import bookRoutes from './books.js';
+import showRoutes from './shows.js';
+import movieRoutes from './movies.js';
+
 const constructorMethod = (app) => {
     // defined routes
-    app.use('/accounts');
+    app.use('/accounts', accountRoutes);
+    app.use('/posts', postRoutes);
+    app.use('/books', bookRoutes);
+    app.use('/shows', showRoutes);
+    app.use('/movies', movieRoutes);
 
     // undefined routes
     app.use('*', (_req, res) => {
