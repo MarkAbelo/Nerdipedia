eaed0e02281dbbd5fa3f52105eaed00a1674066d
KMuldrow <kmuldrow@stevens.edu>
2025-04-22 13:05:07 -0400

    searchShowByTitle and showCard
    functions for searching a show by the given querystring and a page number (currently pages have 20 results) and for getting the show's name+image for the cards have been made. Currently searchShowByTitle gives back the show's ID, title, url, premiere data, and image of 'medium' size if it exists otherwise it's null. This was to simplify the response down to relevant data on each entry as oppose to getShow(id) which returns ALL info the API provides on any given show.
    
    May need to edit searchShowByTitle if it's decided that we need more or less information for search results

--- data-server/data/showsData.js
+++ data-server/data/showsData.js
@@ -58,11 +58,60 @@ const showsDataFunctions = {
     },
 
     async searchShowByTitle(searchTerm, pageNum=1) {
+
+        const itemsPerPage = 20;
         //will give a page of responses from a given input search term
+        //paramter checking
+        if (!searchTerm) throw ('Search term must be provided');
+        if (typeof pageNum !== 'number') throw ('Page must be a number'); 
+        searchTerm = await validationFunctions.validString(searchTerm, "show");
+        //encodes the query 
+        const encodedSearchTerm = encodeURIComponent(searchTerm);
+        //sets the start-end indexes for pagination
+        const startIndex = (pageNum - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        //axios
+        let showsData; 
+        try {
+            const searchResponse = await axios.get(`https://api.tvmaze.com/search/shows?q=${encodedSearchTerm}`);
+            showsData = searchResponse.data
+        } catch(e) {
+            throw(e);
+        }
+        if (showsData == []) {
+            throw ('No shows found!');
+        }
+
+        const showsOnPage = showsData.slice(startIndex, endIndex);
+        if (showsOnPage.length === 0) {
+            throw (`No results found for this page number ${pageNum}`);
+        }
+
+        return showsOnPage.map(elem => {
+            const show = elem.show;
+            return {
+                id: show.id,
+                title: show.name,
+                url: show.url, //the url is an alternative way of running the api call if it can be passed
+                premiered: show.premiered, //YYYY-MM-DD format
+                image: show.image?.medium || null
+            }
+        })
     },
 
     async getShowCard(id) {
         //given a show id, returns the title and image of the show 
+        if (!id) throw ('Id must be provided!');
+        id = await validationFunctions.validPositiveNumber(id, "show ID");
+
+        let showInfo;
+        try {
+            const showResponse = await axios.get(`https://www.tvmaze.com/shows/${id}`)
+            showInfo = showResponse.data;
+        } catch(e) {
+            throw (e);
+        }
+        return {title: showInfo.show.name, image: show.image?.medium || null};
     }
 }
 
