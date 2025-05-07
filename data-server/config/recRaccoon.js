6552206108c59be58f68881db66b2f3866336b32
KP0823 <kushparmar0823@gmail.com>
2025-05-07 18:35:06 -0400

    Racoon was not working before, I think it will work now.... (fingers crossed)

--- data-server/config/recRaccoon.js
+++ data-server/config/recRaccoon.js
@@ -1,16 +1,17 @@
-import Raccoon from '@maruware/raccoon';
+import raccoon from '@maruware/raccoon';
 
-export const bookRec = new Raccoon({
+export const bookRec = new raccoon.default({
     className: 'book',
     redisUrl: 'localhost:6379'
 });
 
-export const showRec = new Raccoon({
+export const showRec = new raccoon.default({
     className: 'show',
     redisUrl: 'localhost:6379'
 });
 
-export const movieRec = new Raccoon({
+export const movieRec =new raccoon.default({
     className: 'movie',
     redisUrl: 'localhost:6379'
 });
+
